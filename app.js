ccd0f57a64f28f1ef501de6282f0908c8b369496
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2018-03-27 15:21:06 +0200

    Add heal endpoint

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const turdRoutes = require("./api/routes/turds");
+const turdRoutes = require('./api/routes/turds');
+const systemRoutes = require('./api/routes/system')
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Methods", "POST, DELETE");
     return res.status(200).json({});
   }
@@ -24,10 +25,11 @@ app.use((req, res, next) => {
 });
 
 // Routes which should handle requests
-app.use("/turds", turdRoutes);
+app.use('/turds', turdRoutes);
+app.use('/health', systemRoutes);
 
 app.use((req, res, next) => {
-  const error = new Error("Not found");
+  const error = new Error('Not found');
   error.status = 404;
   next(error);
 });
